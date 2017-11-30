6634b3e1cd24781ef3c01852a699ccbb13b1952a
Dan Steren <daniel.d.steren@gmail.com>
2017-11-30 04:24:11 -0700

    Add layout. Beautify the login screen. Add drawer to main screen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,102 @@
 import * as React from 'react';
 import AppBar from 'material-ui/AppBar';
+import FAB from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import AccountIcon from 'material-ui/svg-icons/action/account-circle';
 
 import './App.css';
-const logo = require('./logo.svg');
 
-import { MyComp } from './components';
+interface SettingsMenuProps {}
 
-class App extends React.Component {
+const SettingsMenu = (props: SettingsMenuProps) => (
+  <IconMenu
+    {...props}
+    iconButtonElement={
+      <IconButton>
+        <AccountIcon color="white" />
+      </IconButton>
+    }
+    targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+    anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+  >
+    <MenuItem primaryText="Account" />
+    <MenuItem primaryText="Settings" />
+    <MenuItem primaryText="Sign out" />
+  </IconMenu>
+);
+
+interface State {
+  open: boolean;
+}
+
+class App extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+  handleClose = () => this.setState({ open: false });
+
   render() {
     return (
       <div className="App">
-        <AppBar title="My AppBar" />
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <MyComp />
+        <Drawer
+          docked={false}
+          onRequestChange={open => this.setState({ open })}
+          open={this.state.open}
+        >
+          <MenuItem onClick={this.handleClose}>All</MenuItem>
+          <MenuItem onClick={this.handleClose}>Appliances</MenuItem>
+          <MenuItem onClick={this.handleClose}>Arts, Crafts, & Sewing</MenuItem>
+          <MenuItem onClick={this.handleClose}>Automotive</MenuItem>
+          <MenuItem onClick={this.handleClose}>Baby</MenuItem>
+          <MenuItem onClick={this.handleClose}>Beauty & Personal Care</MenuItem>
+          <MenuItem onClick={this.handleClose}>Books</MenuItem>
+          <MenuItem onClick={this.handleClose}>CDs & Vinyl</MenuItem>
+          <MenuItem onClick={this.handleClose}>
+            Cell Phone & Accessories
+          </MenuItem>
+          <MenuItem onClick={this.handleClose}>
+            Collectibles & Fine Art
+          </MenuItem>
+          <MenuItem onClick={this.handleClose}>Computers</MenuItem>
+          <MenuItem onClick={this.handleClose}>Computer Games</MenuItem>
+          <MenuItem onClick={this.handleClose}>Electronics</MenuItem>
+          <MenuItem onClick={this.handleClose}>Health</MenuItem>
+          <MenuItem onClick={this.handleClose}>Home</MenuItem>
+          <MenuItem onClick={this.handleClose}>Luggage & Travel Gear</MenuItem>
+          <MenuItem onClick={this.handleClose}>Miscellaneous</MenuItem>
+          <MenuItem onClick={this.handleClose}>Movies</MenuItem>
+          <MenuItem onClick={this.handleClose}>Musical Instruments</MenuItem>
+          <MenuItem onClick={this.handleClose}>Office Supplies</MenuItem>
+          <MenuItem onClick={this.handleClose}>Patio Lawn & Garden</MenuItem>
+          <MenuItem onClick={this.handleClose}>Pet Supplies</MenuItem>
+          <MenuItem onClick={this.handleClose}>Software</MenuItem>
+          <MenuItem onClick={this.handleClose}>Sports & Outdoors</MenuItem>
+          <MenuItem onClick={this.handleClose}>Tabletop Games</MenuItem>
+          <MenuItem onClick={this.handleClose}>
+            Tools & Home Improvement
+          </MenuItem>
+          <MenuItem onClick={this.handleClose}>Toys</MenuItem>
+          <MenuItem onClick={this.handleClose}>Vehicles</MenuItem>
+          <MenuItem onClick={this.handleClose}>Video Games</MenuItem>
+          <MenuItem onClick={this.handleClose}>Wine</MenuItem>
+        </Drawer>
+        <AppBar
+          onLeftIconButtonTouchTap={() =>
+            this.setState({ open: !this.state.open })
+          }
+          title="Sharedventory"
+          iconElementRight={<SettingsMenu />}
+        />
+        <FAB style={{ position: 'absolute', bottom: 0, right: 0, margin: 25 }}>
+          <ContentAdd />
+        </FAB>
       </div>
     );
   }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,6 +2,10 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
 import { push } from 'react-router-redux';
+import { cyan500, grey100 } from 'material-ui/styles/colors';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 const authSuccess = () => ({
   type: 'AUTH_SUCCESS'
@@ -13,7 +17,48 @@ interface LoginProps extends RouteComponentProps<void> {
 
 class Login extends React.Component<LoginProps, {}> {
   render() {
-    return <button onClick={this.props.login}>Login Here!</button>;
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          backgroundColor: grey100
+        }}
+      >
+        <Paper
+          zDepth={1}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: 400,
+            height: 450,
+            padding: 40
+          }}
+        >
+          <h1 style={{ fontWeight: 400, fontSize: 16, color: cyan500 }}>
+            Sharedventory
+          </h1>
+          <p style={{ fontSize: 24 }}>Sign In</p>
+          <TextField
+            floatingLabelText="Email or Username"
+            style={{ width: 'auto', marginBottom: 50 }}
+          />
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center'
+            }}
+          >
+            <a style={{ color: cyan500, fontSize: 14 }}>More Options</a>
+            <RaisedButton primary label="Sign In" onClick={this.props.login} />
+          </div>
+        </Paper>
+      </div>
+    );
   }
 }
 
