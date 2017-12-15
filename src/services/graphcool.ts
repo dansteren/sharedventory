84b9343b4a7d1b5d423a248bb83d6f624c3f5f3a
Dan Steren <daniel.d.steren@gmail.com>
2017-12-14 20:33:55 -0700

    any login works.

--- src/services/graphcool.ts
+++ src/services/graphcool.ts
@@ -1,16 +1,16 @@
 export const query = (graphqlQuery: string) => {
   return new Promise<QueryResult>(resolve => {
-    if (graphqlQuery.indexOf('asdf') === -1) {
-      resolve({
-        data: 'Fail',
-        errors: [
-          {
-            message: 'I made your request fail, muwahaha',
-            locations: [{ line: 1, column: 1 }]
-          }
-        ]
-      });
-    }
+    // if (graphqlQuery.indexOf('asdf') === -1) {
+    //   resolve({
+    //     data: 'Fail',
+    //     errors: [
+    //       {
+    //         message: 'I made your request fail, muwahaha',
+    //         locations: [{ line: 1, column: 1 }]
+    //       }
+    //     ]
+    //   });
+    // }
     resolve({
       id: '5c0105oucja1k9knt26nrlyng',
       token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MTQ5NjExMjksasdf'
